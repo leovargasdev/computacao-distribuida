0793e2703c5cb8ad7f5d0968e1f6d5a670158e54
LeeonardoVargas <leu1607@hotmail.com>
2018-12-10 09:53:22 -0200

    Començando a separar o cliente e o servidor

---
+++ client.py
@@ -0,0 +1,36 @@
+import requests
+import bottle
+import json
+import threading
+import time
+import sys
+
+peers = sys.argv[2:]
+
+@bottle.route('/add/<p>')
+def index(p):
+    peers.append(p)
+    return json.dumps(peers)
+
+@bottle.route('/peers')
+def index():
+    return json.dumps(peers)
+
+def client():
+    time.sleep(5)
+    while True:
+        time.sleep(1)
+        np = []
+        for p in peers:
+            r = requests.get(p + '/peers')
+            np = np + json.loads(r.text)
+            print(np)
+            time.sleep(1)
+        peers[:] = list(set(np + peers))
+
+        print(peers)
+
+t = threading.Thread(target=client)
+t.start()
+
+bottle.run(host='localhost', port=int(sys.argv[1]))
